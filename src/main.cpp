fadd905037edaa1fe73ccf6fe472814ecf747bb2
runo4938 <runo4938@gmail.com>
2024-02-20 22:46:30 +0300

    updateFromGithub

--- src/main.cpp
+++ src/main.cpp
@@ -42,6 +42,20 @@ WiFiManager wifiManager;
 HTTPClient http;
 WiFiClient client;
 
+//-------OTA---
+/* this info will be read by the python script */
+#define FORMAT_SPIFFS_IF_FAILED true
+
+int currentVersion = 0; // increment currentVersion in each release
+
+String baseUrl = "https://raw.githubusercontent.com/runo4938/src1/master/";
+String checkFile = "update.json";
+/* end of script data */
+
+int fwVersion = 0;
+bool fwCheck = false;
+String fwUrl = "", fwName = "";
+
 TaskHandle_t myTaskHandle = NULL;
 
 int ypos = 190; // position title
@@ -116,7 +130,15 @@ void soundShow();
 void lineondisp();
 int startDayOfWeek(int y, int m, int d);
 void drawCalendar();
-String make_str(String str);
+
+static void rebootEspWithReason(String reason);
+void performUpdate(Stream &updateSource, size_t updateSize);
+void updateFromFS(fs::FS &fs);
+bool downloadFirmware();
+bool checkFirmware();
+
+
+    String make_str(String str);
 String utf8rus(String source);
 String readFile(fs::FS &fs, const char *path);
 
@@ -155,6 +177,33 @@ void setup()
   tft.setCursor(40, 120);
   tft.print(WiFi.SSID());
   delay(2000);
+
+  Serial.println("Firmware Updates from Github");
+  if (!SPIFFS.begin(FORMAT_SPIFFS_IF_FAILED))
+  {
+    Serial.println("SPIFFS Mount Failed");
+    rebootEspWithReason("SPIFFS Mount Failed, rebooting...");
+  }
+  
+    Serial.println("Wifi connected. Checking for updates");
+    if (checkFirmware())
+    {
+      if (SPIFFS.exists("/firmware.bin"))
+      {
+        SPIFFS.remove("/firmware.bin");
+        Serial.println("Removed existing update file");
+      }
+      if (downloadFirmware())
+      {
+        Serial.println("Download complete");
+        updateFromFS(SPIFFS);
+      }
+      else
+      {
+        Serial.println("Download failed");
+      }
+    }
+  
 
   getWeather();
   MessageToScroll_2 = "";
@@ -1879,3 +1928,159 @@ int startDayOfWeek(int y, int m, int d)
   return (y + y / 4 - y / 100 + y / 400 + t[m - 1] + d) % 7;
 }
 /********************************************************/
+static void rebootEspWithReason(String reason)
+{
+  Serial.println(reason);
+  delay(1000);
+  ESP.restart();
+}
+
+void performUpdate(Stream &updateSource, size_t updateSize)
+{
+  String result = "";
+  if (Update.begin(updateSize))
+  {
+    size_t written = Update.writeStream(updateSource);
+    if (written == updateSize)
+    {
+      Serial.println("Written : " + String(written) + " successfully");
+    }
+    else
+    {
+      Serial.println("Written only : " + String(written) + "/" + String(updateSize) + ". Retry?");
+    }
+    result += "Written : " + String(written) + "/" + String(updateSize) + " [" + String((written / updateSize) * 100) + "%] \n";
+    if (Update.end())
+    {
+      Serial.println("OTA done!");
+      result += "OTA Done: ";
+      if (Update.isFinished())
+      {
+        Serial.println("Update successfully completed. Rebooting...");
+        result += "Success!\n";
+      }
+      else
+      {
+        Serial.println("Update not finished? Something went wrong!");
+        result += "Failed!\n";
+      }
+    }
+    else
+    {
+      Serial.println("Error Occurred. Error #: " + String(Update.getError()));
+      result += "Error #: " + String(Update.getError());
+    }
+  }
+  else
+  {
+    Serial.println("Not enough space to begin OTA");
+    result += "Not enough space for OTA";
+  }
+  // http send 'result'
+}
+
+void updateFromFS(fs::FS &fs)
+{
+  File updateBin = fs.open("/firmware.bin");
+  if (updateBin)
+  {
+    if (updateBin.isDirectory())
+    {
+      Serial.println("Error, update.bin is not a file");
+      updateBin.close();
+      return;
+    }
+
+    size_t updateSize = updateBin.size();
+
+    if (updateSize > 0)
+    {
+      Serial.println("Trying to start update");
+      performUpdate(updateBin, updateSize);
+    }
+    else
+    {
+      Serial.println("Error, file is empty");
+    }
+
+    updateBin.close();
+
+    // when finished remove the binary from spiffs to indicate end of the process
+    Serial.println("Removing update file");
+    fs.remove("/firmware.bin");
+
+    rebootEspWithReason("Rebooting to complete OTA update");
+  }
+  else
+  {
+    Serial.println("Could not load update.bin from spiffs root");
+  }
+}
+
+bool downloadFirmware()
+{
+  HTTPClient http;
+  bool stat = false;
+  Serial.println(fwUrl);
+  File f = SPIFFS.open("/firmware.bin", "w");
+  if (f)
+  {
+    http.begin(fwUrl);
+    int httpCode = http.GET();
+    if (httpCode > 0)
+    {
+      if (httpCode == HTTP_CODE_OK)
+      {
+        Serial.println("Downloading...");
+        http.writeToStream(&f);
+        stat = true;
+      }
+    }
+    else
+    {
+      Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+    }
+    f.close();
+  }
+  else
+  {
+    Serial.println("failed to open /firmware.bin");
+  }
+  http.end();
+
+  return stat;
+}
+
+bool checkFirmware()
+{
+  HTTPClient http;
+  http.begin(baseUrl + checkFile);
+  int httpCode = http.GET();
+  bool stat = false;
+  String payload = http.getString();
+  Serial.println(payload);
+  DynamicJsonDocument json(1024);
+  deserializeJson(json, payload);
+  if (httpCode == HTTP_CODE_OK)
+  {
+    fwVersion = json["versionCode"].as<int>();
+    fwName = json["fileName"].as<String>();
+    fwUrl = baseUrl + fwName;
+    if (fwVersion > currentVersion)
+    {
+      Serial.println("Firmware update available");
+      stat = true;
+    }
+    else
+    {
+      Serial.println("You have the latest version");
+    }
+    Serial.print("Version: ");
+    Serial.print(fwName);
+    Serial.print("\tCode: ");
+    Serial.println(fwVersion);
+  }
+  http.end();
+
+  return stat;
+}
